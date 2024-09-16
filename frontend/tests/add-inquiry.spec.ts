b88577889ac54dc6c33234a67bd5c52a81040822
mcverter <mitchell.verter@gmail.com>
2024-09-16 11:31:04 -0400

    fixing some playwright tests

--- frontend/tests/add-inquiry.spec.ts
+++ frontend/tests/add-inquiry.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
+import * as crypto from "crypto";
 
 test.describe('AddInquiry Component', () => {
-  test('should submit a new inquiry', async ({ page }) => {
+  // skipping for now because this will hit the real db
+  test.skip('should submit a new inquiry', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'Why do birds suddenly appear every time you are near?');
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Inquiry created successfully.')).toBeVisible();
@@ -11,14 +13,14 @@ test.describe('AddInquiry Component', () => {
 
   test('should show required error', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Inquiry text is required.')).toBeVisible();
   });
 
   test('should show min length error', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'Why do?');
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Inquiry must be at least 10 characters.')).toBeVisible();
@@ -26,24 +28,25 @@ test.describe('AddInquiry Component', () => {
 
   test('should show max length error', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'Why?'.repeat(100));
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Inquiry can not be greater than 255 characters.')).toBeVisible();
   });
   test('should show capitalization error', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'why do birds suddenly appear every time you are near?');
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Inquiry must start with a capital letter.')).toBeVisible();
   });
-  test('should not allow duplicate inquiries', async ({ page }) => {
+  // skipping for now because this will hit the real db
+  test.skip('should not allow duplicate inquiries', async ({ page }) => {
     await page.goto('/inquiries');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'Why do birds suddenly appear every time you are near?');
     await page.click('button[type="submit"]');
-    await page.click('button[id="add-inquiry-show-modal"]')
+    await page.locator('button').getByText(/Add[\s\n]+Inquiry/).click();
     await page.fill('textarea[name="text"]', 'Why do birds suddenly appear every time you are near?');
     await page.click('button[type="submit"]');
     await expect(page.locator('text=This inquiry already exists.')).toBeVisible();
