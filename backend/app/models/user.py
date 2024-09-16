066cc832401c79544f67f6e78dc1c9c12237d6f5
mcverter <mitchell.verter@gmail.com>
2024-09-16 19:33:54 -0400

    messages is separate

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -70,8 +70,3 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
-
-# Generic message
-class Message(SQLModel):
-    message: str
-
