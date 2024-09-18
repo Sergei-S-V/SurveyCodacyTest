3fbfbc0eb4a273967cb3976b9b44c22d82460a08
mcverter <mitchell.verter@gmail.com>
2024-09-18 15:08:44 -0400

    fixing jest.config.ts

--- frontend/__tests__/inquiry/add_inquiry.test.tsx
+++ frontend/__tests__/inquiry/add_inquiry.test.tsx
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 import {fireEvent, render, screen} from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 import {Inquiries} from "../../src/routes/_layout/inquiries";
