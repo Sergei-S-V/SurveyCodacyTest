f51a4d1c3fde0796ebea1b6889d7bbb025762f79
mcverter <mitchell.verter@gmail.com>
2024-09-20 14:23:56 -0400

    pylint

--- backend/app/tests/services/inquiry/test_create_inquiry.py
+++ backend/app/tests/services/inquiry/test_create_inquiry.py
@@ -8,11 +8,11 @@ from app.services.inquiries import create_inquiry
 
 
 @pytest.fixture
-def mock_session():
+def mock_session() -> Mock:
     return Mock()
 
 
-def test_should_create_with_valid_data(mock_session):
+def test_should_create_with_valid_data(mock_session: Mock) -> None:
     inquiry_data = InquiryCreate(text="Test inquiry")
     expected_inquiry = Inquiry(id=1, text="Test inquiry")
     mock_session.add.return_value = None
@@ -27,27 +27,28 @@ def test_should_create_with_valid_data(mock_session):
     mock_session.refresh.assert_called_once()
 
 
-def test_should_raise_error_with_missing_text(mock_session):
-    with pytest.raises(ValidationError) as ve:
+def test_should_raise_error_with_missing_text(mock_session: Mock) -> None:
+    with pytest.raises(ValidationError, match=r"Field required"):
         inquiry_data = InquiryCreate()
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
-        assert "Validation error for InquiryCreate" in ve.value
 
-    with pytest.raises(ValidationError) as ve:
+    with pytest.raises(ValidationError, match=r"Field required"):
         inquiry_data = InquiryCreate(taxed="Taxed inquiry")
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
-        assert "Validation error for InquiryCreate" in ve.value
 
 
-def test_should_raise_error_with_wrong_text_length(mock_session):
+def test_should_raise_error_with_wrong_text_length(mock_session: Mock) -> None:
     short_string = "A" * 9
     long_string = "A" * 257
-    with pytest.raises(ValidationError) as ve:
+
+    with pytest.raises(
+        ValidationError, match="String should have at least 10 characters"
+    ):
         inquiry_data = InquiryCreate(text=short_string)
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
-        assert "String should have at least 10 characters" in ve.value
 
-    with pytest.raises(ValidationError) as ve:
+    with pytest.raises(
+        ValidationError, match="String should have at most 25. characters"
+    ):
         inquiry_data = InquiryCreate(text=long_string)
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
-        assert "String should have at most 256 characters" in ve.value
