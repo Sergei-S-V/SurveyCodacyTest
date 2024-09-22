c47033a76b1fe38210f2ac01612e44190263576b
mcverter <mitchell.verter@gmail.com>
2024-09-21 20:39:51 -0400

    long names

--- backend/app/tests/services/inquiry/test_create_inquiry.py
+++ backend/app/tests/services/inquiry/test_create_inquiry.py
@@ -12,7 +12,9 @@ def mock_session() -> Mock:
     return Mock()
 
 
-def test_should_create_with_valid_data(mock_session: Mock) -> None:
+def test_create_inquiry_service_should_correctly_invoke_database_commands(
+    mock_session: Mock,
+) -> None:
     inquiry_data = InquiryCreate(text="Test inquiry")
     expected_inquiry = Inquiry(id=1, text="Test inquiry")
     mock_session.add.return_value = None
@@ -27,7 +29,9 @@ def test_should_create_with_valid_data(mock_session: Mock) -> None:
     mock_session.refresh.assert_called_once()
 
 
-def test_should_raise_error_with_missing_text(mock_session: Mock) -> None:
+def test_create_inquiry_service_should_raise_error_when_no_text_parameter_is_given(
+    mock_session: Mock,
+) -> None:
     with pytest.raises(ValidationError, match=r"Field required"):
         inquiry_data = InquiryCreate()
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
@@ -37,15 +41,22 @@ def test_should_raise_error_with_missing_text(mock_session: Mock) -> None:
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
 
 
-def test_should_raise_error_with_wrong_text_length(mock_session: Mock) -> None:
+def test_create_inquiry_service_should_raise_error_when_text_parameter_is_too_short(
+    mock_session: Mock,
+) -> None:
     short_string = "A" * 9
-    long_string = "A" * 257
 
     with pytest.raises(
         ValidationError, match="String should have at least 10 characters"
     ):
         inquiry_data = InquiryCreate(text=short_string)
         create_inquiry(session=mock_session, inquiry_in=inquiry_data)
+
+
+def test_create_inquiry_service_should_raise_error_when_text_parameter_is_too_long(
+    mock_session: Mock,
+) -> None:
+    long_string = "A" * 257
 
     with pytest.raises(
         ValidationError, match="String should have at most 25. characters"
